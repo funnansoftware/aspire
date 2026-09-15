410a16d1baa3899305c518c94c57c7d569d0a360
funnan-dev <funnan@funnansoftware.com>
2026-09-15 15:29:25 -0400

    Implement raylib window wrapper.

--- app/srd-lite/main.cpp
+++ app/srd-lite/main.cpp
@@ -15,54 +15,33 @@ try
 
     constexpr auto windowWidth = 1280;
     constexpr auto windowHeight = 720;
-    constexpr auto targetFramesPerSecond = 60;
-    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-    InitWindow(windowWidth, windowHeight, "Hello Raylib");
-    SetTargetFPS(targetFramesPerSecond);
 
-    constexpr auto gameScreenWidth = 320;
-    constexpr auto gameScreenHeight = 180;
-    const auto target = LoadRenderTexture(gameScreenWidth, gameScreenHeight);
-    const auto bg = Color{.r = 71, .g = 45, .b = 60, .a = 255};
+    aspire::raylib::Window window{windowWidth, windowHeight, "Not D&D Like"};
+    window.setScale(Vector2{.x = 4.0F, .y = 4.0F});
 
     auto object = aspire::parser::json::ReadFile(factory, "D:/dev/aspire/app/srd-lite/database/levels/level_1.json");
     auto* map = dynamic_cast<aspire::raylib::TileMap*>(object.get());
-    map->setPosition(Vector2{.x = gameScreenWidth * 0.5, .y = 0});
+    map->setPosition(Vector2{.x = (window.width() / 4.0F) * 0.5F, .y = 0});
     engine->addChild(object);
 
     auto character = aspire::parser::json::ReadFile(factory, "D:/dev/aspire/app/srd-lite/database/characters/hero.json");
     auto* texture = dynamic_cast<aspire::raylib::Texture*>(character.get());
-    texture->setPosition(Vector2{.x = gameScreenWidth * 0.55, .y = 50});
+    texture->setPosition(Vector2{.x = (window.width() / 4.0F) * 0.55F, .y = 50});
     engine->addChild(character);
 
-    while (!WindowShouldClose())
+    while (!aspire::raylib::Window::shouldClose())
     {
         // Render Game View.
-        BeginTextureMode(target);
-        ClearBackground(bg);
+        const auto bg = Color{.r = 71, .g = 45, .b = 60, .a = 255};
+        window.beginDraw(bg);
 
         for (const auto& [index, drawable] : std::views::enumerate(engine->getChildren<aspire::raylib::Drawable>()))
         {
             drawable->draw();
         }
 
-        EndTextureMode();
-
-        BeginDrawing();
-        ClearBackground(bg);
-
-        DrawTexturePro(
-            target.texture,
-            Rectangle{.x = 0, .y = 0, .width = static_cast<float>(target.texture.width), .height = static_cast<float>(-target.texture.height)},
-            Rectangle{.x = 0, .y = 0, .width = static_cast<float>(GetScreenWidth()), .height = static_cast<float>(GetScreenHeight())},
-            Vector2{.x = 0, .y = 0}, 0, WHITE);
-
-        EndDrawing();
-        PollInputEvents();
-        SwapScreenBuffer();
+        window.endDraw();
     }
-
-    CloseWindow();
 
     return EXIT_SUCCESS;
 }
--- src/raylib/Window.ixx
+++ src/raylib/Window.ixx
@@ -3,19 +3,25 @@ module;
 #include <raylib.h>
 
 export module aspire.raylib.window;
+import aspire.core.object;
 
 export namespace aspire::raylib
 {
-    class Window
+    class Window : public aspire::core::Object
     {
     public:
         Window(int width, int height, const char* title)
         {
+            SetConfigFlags(FLAG_WINDOW_RESIZABLE);
             InitWindow(width, height, title);
+
+            // Initialize the render texture with the current scale.
+            setScale(scale_);
         }
 
         ~Window()
         {
+            UnloadRenderTexture(target_);
             CloseWindow();
         }
 
@@ -25,9 +31,60 @@ export namespace aspire::raylib
         Window(Window&&) = delete;
         auto operator=(Window&&) -> Window& = delete;
 
+        auto beginDraw(Color x) -> void
+        {
+            BeginTextureMode(target_);
+            ClearBackground(x);
+        }
+
+        auto endDraw() -> void
+        {
+            EndTextureMode();
+
+            BeginDrawing();
+            ClearBackground(BLACK);
+
+            DrawTexturePro(
+                target_.texture,
+                Rectangle{.x = 0, .y = 0, .width = static_cast<float>(target_.texture.width), .height = static_cast<float>(-target_.texture.height)},
+                Rectangle{.x = 0, .y = 0, .width = static_cast<float>(width()), .height = static_cast<float>(height())}, Vector2{.x = 0, .y = 0}, 0,
+                WHITE);
+
+            EndDrawing();
+            PollInputEvents();
+            SwapScreenBuffer();
+        }
+
+        auto width() const -> int
+        {
+            return GetScreenWidth();
+        }
+
+        auto height() const -> int
+        {
+            return GetScreenHeight();
+        }
+
+        auto setScale(Vector2 scale) noexcept -> void
+        {
+            scale_ = scale;
+
+            UnloadRenderTexture(target_);
+            target_ = LoadRenderTexture(static_cast<int>(width() / scale_.x), static_cast<int>(height() / scale_.y));
+        }
+
+        [[nodiscard]] auto getScale() const noexcept -> Vector2
+        {
+            return scale_;
+        }
+
         [[nodiscard]] static auto shouldClose() -> bool
         {
             return WindowShouldClose();
         }
+
+    private:
+        RenderTexture2D target_{};
+        Vector2 scale_{1.0, 1.0};
     };
 }
